c2f4eaf1126648a65d0f1dc052603a1fe2bf69ad
X606 <e.forsum@gmail.com>
2021-03-22 15:24:28 +0100

    Added the low gravity mod

---
+++ LowGravity/Main.cs
@@ -0,0 +1,44 @@
+﻿using ModLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LowGravity
+{
+    [MainModClass]
+    public class Main : Mod
+    {
+        public const string SETTINGS_SAVE_ID_GRAVITY = "Gravity";
+
+        public float? NormalGravity = null;
+
+        protected override void OnModRefreshed()
+        {
+            if (NormalGravity == null)
+                NormalGravity = PhysicsManager.Instance.Gravity;
+
+            PhysicsManager.Instance.Gravity = ModdedSettings.GetModdedSettingsIntValue(this, SETTINGS_SAVE_ID_GRAVITY, -10);
+        }
+
+        protected override void OnModDeactivated()
+        {
+            if (NormalGravity == null)
+                return;
+
+            PhysicsManager.Instance.Gravity = NormalGravity.Value;
+        }
+
+        protected override bool ImplementsSettingsWindow() => true;
+
+        protected override void CreateSettingsWindow(ModOptionsWindowBuilder builder)
+        {
+            ModOptionsWindowBuilder.Page page = builder.AddPage("Settings");
+            page.AddIntSlider(-100, 1, -10, "Gravity", SETTINGS_SAVE_ID_GRAVITY, null, null, delegate (int value)
+            {
+                PhysicsManager.Instance.Gravity = (float)value;
+            });
+        }
+    }
+}
---
+++ LowGravity/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LowGravity")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LowGravity")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("be7bfc44-1ca4-48f8-b292-f21983880b57")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
